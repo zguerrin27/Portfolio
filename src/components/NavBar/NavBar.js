82b61706c12d31a343f5bcd3e1754d993ba0d42d
Zachary Guerrin <z.guerrin@gmail.com>
2021-01-16 21:36:32 -0500

    Working on NavBar Stlying and Layout

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,14 +1,42 @@
-import React, {Link} from 'react-router-dom';
+import { Navbar, Nav, } from 'react-bootstrap'
+import './NavBar.css';
 
 function NavBar(props) {
   return (
-    <div>
-      <Link to="/">Home</Link>
-      <Link to="/about">About</Link>
-      <Link to="/projects">Projects</Link>
-      <Link to="/resume">Resume</Link>
-      <Link to="/contact">Contact</Link>
-    </div>
+    <Navbar bg="dark" variant="dark" expand="sm">
+      <Navbar.Brand href="/">ZacharyGuerrin</Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="ml-auto">                          {/* means "marginLeft-auto". could also be mr-auto */}
+          <Nav.Link href="/about">About Me</Nav.Link>
+          <Nav.Link href="/projects">Projects</Nav.Link>
+          <Nav.Link href="/resume">Resume</Nav.Link>
+          <Nav.Link href="/contact">Contact Me</Nav.Link>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
+    // <container className="nav__container">
+    //   <Nav className="name__left" activeKey="/">
+    //     <Nav.Item>
+    //       <Nav.Link href="/">ZachGuerrin</Nav.Link>
+    //     </Nav.Item>
+    //   </Nav>
+    //   <Nav className="nav__right" activeKey="/">
+    //     <Nav.Item>
+    //       <Nav.Link href="/about" eventKey="link-1">About Me</Nav.Link>
+    //     </Nav.Item>
+    //     <Nav.Item>
+    //       <Nav.Link href="/projects" eventKey="link-2">Projects</Nav.Link>
+    //     </Nav.Item>
+    //     <Nav.Item>
+    //       <Nav.Link href="/resume" eventKey="link-3" >Resume</Nav.Link>
+    //     </Nav.Item>
+    //     <Nav.Item>
+    //       <Nav.Link href="/contact" eventKey="link-4" >Contact Me</Nav.Link>
+    //     </Nav.Item>
+    //   </Nav>
+    // </container>
+ 
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import ReactDOM from 'react-dom';
 import './index.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 import App from './App';
 
 ReactDOM.render(
