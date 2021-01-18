bd2bf37bbbe15d5454b2034175704856ff2e3e30
Zachary Guerrin <z.guerrin@gmail.com>
2021-01-18 10:11:06 -0500

    Added styles and icons to navbar

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -3,40 +3,23 @@ import './NavBar.css';
 
 function NavBar(props) {
   return (
-    <Navbar bg="dark" variant="dark" expand="sm">
+    <Navbar bg="dark" variant="dark" expand="md">
       <Navbar.Brand href="/">ZacharyGuerrin</Navbar.Brand>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
-        <Nav className="ml-auto">                          {/* means "marginLeft-auto". could also be mr-auto */}
+        <Nav className="mr-auto left-nav">                          {/* means "marginLeft-auto". could also be mr-auto */}
           <Nav.Link href="/about">About Me</Nav.Link>
           <Nav.Link href="/projects">Projects</Nav.Link>
           <Nav.Link href="/resume">Resume</Nav.Link>
           <Nav.Link href="/contact">Contact Me</Nav.Link>
         </Nav>
+        <Nav className="ml-auto right-nav">                         
+          <Nav.Link href="/"><i class="fab fa-github fa-2x" ></i></Nav.Link>
+          <Nav.Link href="/"><i class="fab fa-linkedin fa-2x" ></i></Nav.Link>
+          <Nav.Link href="/"><i class="fab fa-instagram fa-2x" ></i></Nav.Link>
+        </Nav>
       </Navbar.Collapse>
     </Navbar>
-    // <container className="nav__container">
-    //   <Nav className="name__left" activeKey="/">
-    //     <Nav.Item>
-    //       <Nav.Link href="/">ZachGuerrin</Nav.Link>
-    //     </Nav.Item>
-    //   </Nav>
-    //   <Nav className="nav__right" activeKey="/">
-    //     <Nav.Item>
-    //       <Nav.Link href="/about" eventKey="link-1">About Me</Nav.Link>
-    //     </Nav.Item>
-    //     <Nav.Item>
-    //       <Nav.Link href="/projects" eventKey="link-2">Projects</Nav.Link>
-    //     </Nav.Item>
-    //     <Nav.Item>
-    //       <Nav.Link href="/resume" eventKey="link-3" >Resume</Nav.Link>
-    //     </Nav.Item>
-    //     <Nav.Item>
-    //       <Nav.Link href="/contact" eventKey="link-4" >Contact Me</Nav.Link>
-    //     </Nav.Item>
-    //   </Nav>
-    // </container>
- 
   );
 }
 
