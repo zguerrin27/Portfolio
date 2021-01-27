69d4a073735d3bbcbfa1084e7e774cb161d7693e
Zachary Guerrin <z.guerrin@gmail.com>
2021-01-27 18:00:45 -0500

    Finished contact form send

--- src/components/Form/ContactForm.js
+++ src/components/Form/ContactForm.js
@@ -1,17 +1,29 @@
 import React from 'react';
 import { useForm } from "react-hook-form";
+import emailjs from 'emailjs-com';
 
 import './ContactForm.css';
 
 
 function ContactForm(props) {
-
   const { register, handleSubmit, errors } = useForm();
-  const onSubmit = data => console.log(data);
+  const sendEmail = (e) => {
+    emailjs.send(process.env.REACT_APP_SERVICE_ID, process.env.REACT_APP_TEMPLATE_ID, e, process.env.REACT_APP_USER_ID)
+      .then((result) => {
+        // eslint-disable-next-line no-restricted-globals
+        if(confirm('Message Sent, I\'ll get back to you shortly', result.text)){
+          window.location.reload();  
+        }
+        // alert('Message Sent, I\'ll get back to you shortly', result.text);
+      }, (error) => {
+        alert( 'An error occured, Plese try again', error.text)
+      });
+      
+  }
 
   return (
-    // <div className="contactForm__Container">
-      <form onSubmit={handleSubmit(onSubmit)}>
+
+      <form onSubmit={handleSubmit(sendEmail)}>
         <h1 className="contact__header">Contact Me.</h1>
           <div>
             <label htmlFor="firstName">First Name</label>
@@ -57,18 +69,18 @@ function ContactForm(props) {
           <div>
             <label htmlFor="message">Enter a message</label>
             <textarea 
-              name="textArea" 
+              name="message" 
               placeholder="Hey Zach! Lets work together..."
               className="textArea"
               ref={register({
                 required: "A message is required."
               })} />
-            {errors.textArea && <p className="errorMsg">{errors.textArea.message}</p>}
+            {errors.message && <p className="errorMsg">{errors.message.message}</p>}
           </div>
           <input type="submit" />
         </form>
         
-      // </div>
+
   );
 }
 
